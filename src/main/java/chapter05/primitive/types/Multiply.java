434efc732c4565a68c3491a56429689ec033743d
Ajay Padvi <ajay.padvi@smartstream-stp.com>
2018-06-26 09:21:39 +0530

    Implemented multiply of long using bitwise operators only

--- src/main/java/chapter05/primitive/types/Multiply.java
+++ src/main/java/chapter05/primitive/types/Multiply.java
@@ -1,4 +1,18 @@
 package chapter05.primitive.types;
 
 public class Multiply {
+
+    public static long multiply(long x, long y) {
+        long sum = 0;
+        while (x != 0) {
+            //Examine each bit x
+            if ((x & 1) != 0) {
+                sum = Add.add(sum, y);
+            }
+
+            x >>>= 1;
+            y <<= 1;
+        }
+        return sum;
+    }
 }
--- src/test/java/MultiplyTest.java
+++ src/test/java/MultiplyTest.java
@@ -1,2 +1,34 @@
+import chapter05.primitive.types.Multiply;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static chapter05.primitive.types.Multiply.multiply;
+import static org.junit.Assert.*;
+
 public class MultiplyTest {
+
+    @Test
+    public void test() {
+        long a = 13;
+        long b = 9;
+        long expected = a * b;
+        long result = multiply(a, b);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testRandom() {
+        Random r = new Random();
+        // Random test, only works if the product is not greater than 2^32 - 1.
+        for (int i = 0; i < 100000; ++i) {
+            long x = r.nextInt(65535), y = r.nextInt(65535);
+            //long x = r.nextInt(),y=r.nextInt(); This works
+            //long x = r.nextLong(),y=r.nextLong(); This also works
+            long prod = multiply(x, y);
+            assertEquals((long) x * y, prod);
+            System.out.println("PASS: x = " + x + ", y = " + y + "; prod = "
+                    + prod);
+        }
+    }
 }
